f926cc2bf3758057cc589bdb115a544e5e12a7d1
Sad Pencil <qh06@qq.com>
2019-07-10 14:51:34 +0800

    rename

--- Experiments/run_bootnode.sh
+++ Experiments/run_bootnode.sh
@@ -8,4 +8,4 @@ screen -S pride_bootnode -X quit >/dev/null 2>&1 || true
 killall bootnode >/dev/null 2>&1 || true
 
 # run server at background
-screen -Sdm pride_bootnode ./bin/bootnode -nodekey ./boot.key -verbosity 9 -addr :30310
+screen -Sdm pride_bootnode ./bin/bootnode -nodekey ./bootnode.key -verbosity 9 -addr :30310
