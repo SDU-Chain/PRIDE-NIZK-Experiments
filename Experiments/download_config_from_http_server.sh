f1ce6511531966179937146f2c8b779f7ac7d480
Sad Pencil <qh06@qq.com>
2019-07-11 16:02:34 +0800

    http download genesis

--- Experiments/download_config_from_http_server.sh
+++ Experiments/download_config_from_http_server.sh
@@ -4,3 +4,4 @@ wget `cat ./config/httpserver_url`/config/bootnode -O - | tee ./config/bootnode
 wget `cat ./config/httpserver_url`/config/contract -O - | tee ./config/contract
 rm -rf ./gethaccounts/cars || true
 wget `cat ./config/httpserver_url`/gethaccounts/cars.tar.gz -O - | tar -xz -C ./gethaccounts
+wget `cat ./config/httpserver_url`/genesis.json -O - > ./genesis.json
--- Experiments/init_geth.sh
+++ Experiments/init_geth.sh
@@ -5,6 +5,7 @@ pushd ./gethdata
 rm geth.ipc || true
 rm -rf geth/ || true
 popd
+cp ./genesis.json ./http/genesis.json
 ./bin/geth-timing --datadir ./gethdata init ./genesis.json
 
 
