ae20f8c035b4b5ba403c5deb7f091e6734fd9750
Sad Pencil <qh06@qq.com>
2019-07-24 17:16:01 +0800

    Update run_experiment_2.sh

--- Experiments/run_experiment_2.sh
+++ Experiments/run_experiment_2.sh
@@ -4,3 +4,13 @@ for i in $( seq 1 $count )
 do
 	echo 100 | ./run_car_emulator.sh &
 done
+
+while true; do
+	num=`pidof car | tr " " "\n" | wc -l`
+	echo "Number of cars remaining: $num"
+	if [ $num -eq 0 ]; then
+		echo "No cars remaining. Exited."
+		exit 0
+	fi
+	sleep 1
+done
