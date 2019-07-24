380d13fd4eb73cfd69c31ae4e92a0705a65c7bfd
Sad Pencil <qh06@qq.com>
2019-07-24 20:55:04 +0800

    update exp2

--- Experiments/run_experiment_2.sh
+++ Experiments/run_experiment_2.sh
@@ -3,13 +3,13 @@ read -p "Number of cars=? " count
 for i in $( seq 1 $count )
 do
 	echo 100 | ./run_car_emulator.sh 1>/dev/null 2>/dev/null &
-	echo -n "Starting car ($i/$count)"
+	echo -n "Starting car ($i/$count)             "
 	printf "\r"
 done
 
 while true; do
 	num=`pidof car | tr " " "\n" | wc -l`
-	echo -n "Number of cars remaining: $num"
+	echo -n "Number of cars remaining: $num       "
 	printf "\r"
 	if [ $num -eq 0 ]; then
 		echo "No cars remaining. Exited."
