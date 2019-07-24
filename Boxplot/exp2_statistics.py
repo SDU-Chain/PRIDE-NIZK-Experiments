2c6d2aa2e89d08005e9a9186ac0e3e40a313a17e
Sad Pencil <qh06@qq.com>
2019-07-24 20:54:28 +0800

    Update exp2_statistics.py

--- Boxplot/exp2_statistics.py
+++ Boxplot/exp2_statistics.py
@@ -5,16 +5,6 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import pandas as pd
 
-
-def status(list):
-    x = pd.Series((v[0] for v in list))
-    return pd.Series([x.count(), x.min(), x.idxmin(), x.quantile(.25), x.median(),
-                      x.quantile(.75), x.mean(), x.max(), x.idxmax(), x.mad(), x.var(),
-                      x.std(), x.skew(), x.kurt()], index=['总数', '最小值', '最小值位置', '25%分位数',
-                                                           '中位数', '75%分位数', '均值', '最大值', '最大值位数', '平均绝对偏差', '方差', '标准差',
-                                                           '偏度', '峰度'])
-
-
 if __name__ == "__main__":
 
     # Read Excel
@@ -63,23 +53,51 @@ if __name__ == "__main__":
     sheet = workbook.add_worksheet('Worksheet')
 
     row_id = 0
-    sheet.write(row_id, 0, 'filename')
-    sheet.write(row_id, 1, '25%')
-    sheet.write(row_id, 2, '50%')
-    sheet.write(row_id, 3, '75%')
-    sheet.write(row_id, 4, '95%')
-    sheet.write(row_id, 5, '99%')
-    sheet.write(row_id, 6, 'average')
+    col_id = 0
+    sheet.write(row_id, col_id, 'filename')
+    col_id += 1
+    sheet.write(row_id, col_id, '25%')
+    col_id += 1
+    sheet.write(row_id, col_id, '50%')
+    col_id += 1
+    sheet.write(row_id, col_id, '85%')
+    col_id += 1
+    sheet.write(row_id, col_id, '86%')
+    col_id += 1
+    sheet.write(row_id, col_id, '87%')
+    col_id += 1
+    sheet.write(row_id, col_id, '88%')
+    col_id += 1
+    sheet.write(row_id, col_id, '89%')
+    col_id += 1
+    sheet.write(row_id, col_id, '90%')
+    col_id += 1
+    sheet.write(row_id, col_id, 'average')
+    col_id += 1
 
     for k in sorted(group_verproof_execution_times.keys()):
         df = pd.DataFrame(group_verproof_execution_times[k])
         row_id += 1
-        sheet.write(row_id, 0, k)
-        sheet.write(row_id, 1, df.quantile(.25)[0])
-        sheet.write(row_id, 2, df.quantile(.50)[0])
-        sheet.write(row_id, 3, df.quantile(.75)[0])
-        sheet.write(row_id, 4, df.quantile(.95)[0])
-        sheet.write(row_id, 5, df.quantile(.99)[0])
-        sheet.write(row_id, 6, df.mean()[0])
+        col_id = 0
+        sheet.write(row_id, col_id, k)
+        col_id += 1
+        sheet.write(row_id, col_id, df.quantile(.25)[0])
+        col_id += 1
+        sheet.write(row_id, col_id, df.quantile(.50)[0])
+        col_id += 1
+        sheet.write(row_id, col_id, df.quantile(.85)[0])
+        col_id += 1
+        sheet.write(row_id, col_id, df.quantile(.86)[0])
+        col_id += 1
+        sheet.write(row_id, col_id, df.quantile(.87)[0])
+        col_id += 1
+        sheet.write(row_id, col_id, df.quantile(.88)[0])
+        col_id += 1
+        sheet.write(row_id, col_id, df.quantile(.89)[0])
+        col_id += 1
+        sheet.write(row_id, col_id, df.quantile(.90)[0])
+        col_id += 1
+        sheet.write(row_id, col_id, df.mean()[0])
+        col_id += 1
 
     workbook.close()
